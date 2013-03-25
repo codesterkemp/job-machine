54c0bcd51c65382c837591b7f1b30135e36e8092
joshua kemp <joshuakemp85@gmail.com>
2013-03-25 14:05:19 -0400

    rooting Home as our index in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Jobmachine::Application.routes.draw do
+  
+  root :to => "static_pages#home"  
+
   get "static_pages/home"
 
   get "static_pages/help"
 
   get "static_pages/about"
+
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +59,7 @@ Jobmachine::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
